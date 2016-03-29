09d5a78b1632459c3e9bd37cc9f804a72070faf0
Dave Cooper <dave@spudooli.com>
2016-03-30 10:44:28 +1300

    Adds some API calls to send the temperatures

--- making-pizza.py
+++ making-pizza.py
@@ -7,7 +7,9 @@ import Adafruit_MAX31855.MAX31855 as MAX31855
 # https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code
 from Adafruit_7Segment import SevenSegment
 
+import urllib2
 
+pizzaapi = "https://www.spudooli.com/track"
 
 segment = SevenSegment(address=0x70)
 
@@ -30,3 +32,7 @@ segment.writeDigit(0, int(hour / 10))
 segment.writeDigit(1, hour % 10) 
 segment.writeDigit(3, int(minute / 10))
 segment.writeDigit(4, minute % 10)
+
+pizzapi = pizzaapi + "?insidetemp=" + pizza-oven-temperature + "&outsidetemp=" + internal
+
+urllib2.urlopen(pizzapi)
