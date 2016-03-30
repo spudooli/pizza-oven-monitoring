34743068724873821eca9f02b1c4b58c2e2c5540
Dave Cooper <dave@spudooli.com>
2016-03-30 13:14:05 +1300

    Things

--- making-pizza.py
+++ making-pizza.py
@@ -9,7 +9,7 @@ from Adafruit_7Segment import SevenSegment
 
 import urllib2
 
-pizzaapi = "https://www.spudooli.com/track"
+pizzaapi = "https://www.spudooli.com/track/"
 
 segment = SevenSegment(address=0x70)
 
@@ -22,8 +22,8 @@ sensor = MAX31855.MAX31855(CLK, CS, DO)
 
 pizza-oven-temperature = sensor.readTempC()
 internal = sensor.readInternalC()
-print 'Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(pizza-oven-temperature)
-print '    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal)
+print 'Thermocouple Temperature: {0:0.3F}*C'.format(pizza-oven-temperature)
+print '    Internal Temperature: {0:0.3F}*C'.format(internal)
 
 
 
